d6ca1a63de130ffc81bd3450191bc3f091ce36ee
Manjiri Vadujkar <manjiri.vadujkar@gmail.com>
2022-04-10 18:14:39 +0530

    Initial Commit

---
+++ movie-website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <navbar class="nav">
+        <h2> Navbar </h2>
+      </navbar>
+    </div>
+  );
+}
+
+export default App;
